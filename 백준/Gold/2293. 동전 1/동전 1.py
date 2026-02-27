a66199a94f235e7d3061c1539dc529dcdf92542a
Seong-min An <120103442+S-u-n-g@users.noreply.github.com>
2026-02-27 16:14:10 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 111812 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,17 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+coins = []
+
+for _ in range(n):
+    coins.append(int(sys.stdin.readline()))
+
+# dp[i]는 i원을 만드는 경우의 수
+dp = [0] * (k+1)
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] += dp[i - coin]
+
+print(dp[-1])
