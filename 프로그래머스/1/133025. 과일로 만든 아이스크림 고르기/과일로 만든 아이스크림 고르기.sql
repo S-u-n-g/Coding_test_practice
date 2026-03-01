e3adb1b80f319bfe9a1c4b39473d677480d40805
Seong-min An <120103442+S-u-n-g@users.noreply.github.com>
2026-03-01 23:00:51 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,4 @@
+SELECT FIRST_HALF.FLAVOR from FIRST_HALF inner join ICECREAM_INFO 
+on FIRST_HALF.flavor = ICECREAM_INFO.flavor
+where TOTAL_ORDER > 3000 and INGREDIENT_TYPE = 'fruit_based'
+order by TOTAL_ORDER desc
